d9ab072398abc72656a8fe98212046f3166deb10
Artur Rok <rokartur520@gmail.com>
2023-12-23 18:28:34 +0100

    Initial Commit

---
+++ Micmute/MicmuteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicmuteApp.swift
+//  Micmute
+//
+//  Created by rokartur on 23/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MicmuteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
