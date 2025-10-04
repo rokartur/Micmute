ef30d4d8a8b1c608d1a5b21730d9721106631d87
rokartur <rokartur@icloud.com>
2025-10-04 20:50:10 +0200

    CLOSE SOURCE

--- Micmute/Micmute-Bridging-Header.h
+++
@@ -1,7 +0,0 @@
-#ifndef Micmute_Bridging_Header_h
-#define Micmute_Bridging_Header_h
-
-#include "Audio/Driver/VirtualAudioDriverBridge.h"
-#include "Audio/Installer/AuthorizationHelper.h"
-
-#endif /* Micmute_Bridging_Header_h */
--- Micmute/Utilities/UpdaterService+MicmuteFix.swift
+++
@@ -1,1 +0,0 @@
-// Legacy updater hook removed.
